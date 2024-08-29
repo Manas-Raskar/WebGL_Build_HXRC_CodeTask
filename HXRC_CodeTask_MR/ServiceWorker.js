f0e5849da9d0e95e82291e55780678fe9f314ffa
Manas-Raskar <162117250+Manas-Raskar@users.noreply.github.com>
2024-08-29 14:23:41 +0200

    Add files via upload

---
+++ HXRC_CodeTask_MR/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-SwitchColor-1.9.9";
+const contentToCache = [
+    "Build/HXRC_CodeTask_MR.loader.js",
+    "Build/HXRC_CodeTask_MR.framework.js",
+    "Build/HXRC_CodeTask_MR.data",
+    "Build/HXRC_CodeTask_MR.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
